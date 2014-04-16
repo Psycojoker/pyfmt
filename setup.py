c7e0473af63a370e8b2ac0f000e2d5f7abcd23e9
Laurent Peuch <cortex@worlddomination.be>
2014-04-16 23:08:37 +0200

    [enh] add a setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from setuptools import setup
+
+
+setup(name='pyfmt',
+      version='0.1',
+      description='automatic code formatter for python following pep8 using baron FST, like gofmt',
+      author='Laurent Peuch',
+      #long_description='',
+      author_email='cortex@worlddomination.be',
+      url='https://github.com/Psycojoker/pyfmt',
+      install_requires=['baron'],
+      license= 'gplv3+',
+      keywords='pep8 formatting baron fst code',
+      entry_points={
+        'console_scripts': [
+            'pyfmt = pyfmt:main',
+        ]
+      }
+     )
