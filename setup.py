0bd42fdb20842204319aa15e4d70cd80f5ce77df
Nauman Ahmad <nauman-ahmad@outlook.com>
2014-09-07 09:49:30 +0500

    PEP 8 Fixes

--- setup.py
+++ setup.py
@@ -8,16 +8,16 @@ setup(name='pyfmt',
       version='0.1',
       description='automatic code formatter for python following pep8 using baron FST, like gofmt',
       author='Laurent Peuch',
-      #long_description='',
+      # long_description='',
       author_email='cortex@worlddomination.be',
       url='https://github.com/Psycojoker/pyfmt',
       install_requires=['baron'],
-      license= 'gplv3+',
+      license='gplv3+',
       keywords='pep8 formatting baron fst code',
       py_modules=['pyfmt'],
       entry_points={
-        'console_scripts': [
-            'pyfmt = pyfmt:main',
-        ]
+      'console_scripts': [
+          'pyfmt = pyfmt:main',
+      ]
       }
-     )
+      )
