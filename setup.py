fe160875eb9fb10b9a2f959256f5f293c7dde50d
Laurent Peuch <cortex@worlddomination.be>
2014-09-07 11:04:04 +0200

    [enh] handle long description in the README.md using pypandoc trick

--- setup.py
+++ setup.py
@@ -3,12 +3,19 @@
 
 from setuptools import setup
 
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
 
 setup(name='pyfmt',
       version='0.1',
       description='automatic code formatter for python following pep8 using baron FST, like gofmt',
       author='Laurent Peuch',
-      # long_description='',
+      long_description=read_md("README.md") + "\n\n" + open("CHANGELOG", "r").read(),
       author_email='cortex@worlddomination.be',
       url='https://github.com/Psycojoker/pyfmt',
       install_requires=['baron<0.4,<=0.3'],
