bd9688bbed8ff88626c73d6a3e184e65a22c477d
Laurent Peuch <cortex@worlddomination.be>
2014-11-14 08:04:08 +0100

    [enh] pass associative parenthesis test

--- pyfmt.py
+++ pyfmt.py
@@ -747,6 +747,12 @@ empty_string = lambda _: ""
 
 
 custom_key_renderers = {
+    "associative_parenthesis": {
+        "first_formatting": empty_string,
+        "second_formatting": empty_string,
+        "third_formatting": empty_string,
+        "fourth_formatting": empty_string,
+    },
     "call": {
         "first_formatting": empty_string,
         "second_formatting": empty_string,
